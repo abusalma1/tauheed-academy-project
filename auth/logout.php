3dea045e77241148ab0e066730a8a36e6de367dd
Abubakar Ahmad Adili <abubakarahmadadili@gmail.com>
2025-10-15 12:48:33 +0100

    feat(config): created algroithm & database_schema

--- auth/logout.php
+++ auth/logout.php
@@ -1,8 +1,10 @@
 <?php
-
 session_start();
 
-
-unset($_SESSION["user_id"]);
-unset($_SESSION["user_email"]);
-header("Location: ./login.php");
+if (isset($_SESSION["user_id"]) &&  isset($_SESSION["user_email"])) {
+    unset($_SESSION["user_id"]);
+    unset($_SESSION["user_email"]);
+    header("Location: ./login.php");
+} else {
+    header('Location: index.php');
+}
